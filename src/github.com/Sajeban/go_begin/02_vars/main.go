19df88286731d49c0c552f1a10960191c051c94a
Sajeban rex <antonyrex.2017472@iit.ac.lk>
2019-06-02 02:10:32 +0545

    Add existing file

---
+++ src/github.com/Sajeban/go_begin/02_vars/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 string = "Saje"
+	var name2 = "Sajeban" //var name string="Sajeban" is also correct but using inverted comma defines it as string
+	var age1 int = 21     //In go if you create a variable it must be used or it will throw error
+	var age2 = 22
+	var isStudent = true
+	const pi = 3.14
+	fmt.Println(name1, age1, name2, age2, isStudent)
+	fmt.Printf("%T\n", pi) //To find the data type of a variable
+}
