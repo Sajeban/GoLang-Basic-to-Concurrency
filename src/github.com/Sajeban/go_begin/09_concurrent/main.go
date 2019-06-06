72b6313b97f9e5c90f590a37a7c5d2c5f03424b0
Sajeban rex <antonyrex.2017472@iit.ac.lk>
2019-06-06 23:42:48 +0545

    Add existing file

--- src/github.com/Sajeban/go_begin/09_concurrent/main.go
+++ src/github.com/Sajeban/go_begin/09_concurrent/main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
+	c := make(chan int)
+	go print("Sajeban", c)
+	// for {
+	// 	msg, open := <-c
+	// 	fmt.Println("main", msg)
+	// 	if !open {
+	// 		break
+	// 	}
 
-	go print("Sajeban")
-	print("Vaksh")
+	// }
+	for msg := range c {
+		fmt.Println(msg)
+	}
 }
-func print(x string) {
+func print(x string, c chan int) {
 	for i := 0; i <= 5; i++ {
-		fmt.Println(x)
+		c <- i
+		fmt.Println("routine ", x)
 		time.Sleep(time.Millisecond * 500)
 	}
+	close(c)
 }
